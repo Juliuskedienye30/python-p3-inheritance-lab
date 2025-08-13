86282d7808b8fc322b747e04251eaf7d656e6869
Julius Kedienye <juliuskedienye61@gmail.com>
2025-08-13 21:24:40 +0300

    first commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,49 @@
 #!/usr/bin/env python
 
+# Import the User base class
 from user import User
 
+# Import Python's random module to help select random knowledge
 import random
 
 class Teacher(User):
+    """
+    Teacher class that inherits from User and can 'teach'
+    by returning a random piece of predefined knowledge.
+    """
+
+    def __init__(self, first_name, last_name):
+        """
+        Initialize a Teacher object with:
+        - first_name and last_name (inherited from User)
+        - a predefined list of knowledge strings
+        """
+
+        # Call the __init__ method of the parent (User) class
+        # This ensures that first_name and last_name are set
+        # without having to rewrite the same code here.
+        super().__init__(first_name, last_name)
+
+        # Store a list of knowledge topics the teacher can teach
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+        """
+        Select and return a random piece of knowledge
+        from the teacher's knowledge list.
+        """
+
+        # Get a random index between 0 and the last index of the list
+        random_index = random.randint(0, len(self.knowledge) - 1)
+
+        # Return the knowledge at that random position
+        return self.knowledge[random_index]
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    """
+    A generic user with a first name and last name.
+    This is the base class for Student and Teacher.
+    """
+
+    def __init__(self, first_name, last_name):
+        """
+        Initialize a User with a first name and last name.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def full_name(self):
+        """
+        Return the full name of the user.
+        """
+        return f"{self.first_name} {self.last_name}"
